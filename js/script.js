17cf60089af338ee40f3c21a18825bb52bcdbc1b
Indicattis <joao.staehler@gmail.com>
2023-03-01 14:32:32 -0300

    Importação para github

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// $('#header--slider').slick({
+//     dots: false,
+//     infinite: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: false,
+//     autoplaySpeed: 1500
+//   });
+  $('#menu--toggle').click(function () {
+    $('.header--mobile').toggleClass("show")
+  }
+  )
+  $('#table--button--filter--list').click(function () {
+    $('#table--button--filter--list').toggleClass('selected');
+    $('#table--button--filter--grid').removeClass('selected');
+    $('.sec--room--grid').removeClass('change');
+    $('.sec--room--table').removeClass('change');
+  })
+  $('#table--button--filter--grid').click(function () {
+    $('#table--button--filter--grid').toggleClass('selected');
+    $('#table--button--filter--list').removeClass('selected');
+    $('.sec--room--grid').toggleClass('change');
+    $('.sec--room--table').toggleClass('change');
+  })
