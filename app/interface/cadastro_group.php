8734a6304a10457abb13b9ad6ee3fe048d3f3953
Indicattis <joao.staehler@gmail.com>
2023-04-11 14:31:22 -0300

    Adição de grupo funcionalizada

---
+++ app/interface/cadastro_group.php
@@ -0,0 +1,54 @@
+<?php
+include_once('../data/conect.php');
+include_once('../log/log.php');
+session_start();
+
+$group_name = $_POST['groupName'];
+$unit_name = $_POST['groupUnit'];
+
+$insertGroup = mysqli_query($conect,"INSERT INTO groups(
+    
+    group_name, 
+    unit_name)
+    VALUES ( 
+        '$group_name',
+        '$unit_name')");
+
+if($insertGroup) {
+    echo "
+    <div class='request--feedback--info'>
+        <div class='modal--legend'>
+            <legend>Aviso!</legend>
+        </div>
+        <div class='box--line'>
+            <div class='box--col'>
+                <P>Cadastro realizado!</P>
+            </div>
+        </div>
+        <div class='box--line'>
+            <button class='button--submit request--feedback--button' onclick='closeModal()'> OK </button>
+        </div>
+    </div>
+    ";
+    logger($_SESSION['mail'].' added a new group: '.$group_name, $mode = 'NOTICE');
+    LogToDatabase('added a new group: '.$group_name, $mode = 'NOTICE');
+}
+else {
+    echo "
+    <div class='request--feedback--info'>
+        <div class='modal--legend'>
+            <legend>Aviso!</legend>
+        </div>
+        <div class='box--line'>
+            <div class='box--col'>
+                <P>Erro ao cadastrar!</P>
+            </div>      
+        </div>
+        <div class'box--line'>
+            <button class='button--submit request--feedback--button' onclick='closeModal()'> OK </button>
+        </div>
+    </div>
+    ";
+    logger($_SESSION['mail']. ' error on added a new group: '.$group_name, $mode = 'ERROR');
+    LogToDatabase('error on added a new group: '.$group_name, $mode = 'ERROR');
+}
